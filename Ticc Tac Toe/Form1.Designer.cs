5676c9d35a9e862d7d9439b73f9d4a909951abcb
ActuallyMe-trust <actuallyme136@gmail.com>
2026-09-12 19:28:46 +0800

    Initial UI Design

---
+++ Ticc Tac Toe/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Ticc_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            playerscore = new Label();
+            cpuscore = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.YellowGreen;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button1.ForeColor = Color.FloralWhite;
+            button1.Location = new Point(143, 144);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(111, 91);
+            button1.TabIndex = 0;
+            button1.Text = "X";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.ForestGreen;
+            button2.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button2.ForeColor = Color.FloralWhite;
+            button2.Location = new Point(263, 144);
+            button2.Margin = new Padding(4);
+            button2.Name = "button2";
+            button2.Size = new Size(111, 91);
+            button2.TabIndex = 1;
+            button2.Text = "X";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.YellowGreen;
+            button3.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button3.ForeColor = Color.FloralWhite;
+            button3.Location = new Point(383, 144);
+            button3.Margin = new Padding(4);
+            button3.Name = "button3";
+            button3.Size = new Size(111, 91);
+            button3.TabIndex = 2;
+            button3.Text = "X";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.ForestGreen;
+            button4.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button4.ForeColor = Color.FloralWhite;
+            button4.Location = new Point(143, 243);
+            button4.Margin = new Padding(4);
+            button4.Name = "button4";
+            button4.Size = new Size(111, 91);
+            button4.TabIndex = 3;
+            button4.Text = "X";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.YellowGreen;
+            button5.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button5.ForeColor = Color.FloralWhite;
+            button5.Location = new Point(263, 243);
+            button5.Margin = new Padding(4);
+            button5.Name = "button5";
+            button5.Size = new Size(111, 91);
+            button5.TabIndex = 4;
+            button5.Text = "X";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.ForestGreen;
+            button6.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button6.ForeColor = Color.FloralWhite;
+            button6.Location = new Point(383, 243);
+            button6.Margin = new Padding(4);
+            button6.Name = "button6";
+            button6.Size = new Size(111, 91);
+            button6.TabIndex = 5;
+            button6.Text = "X";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.YellowGreen;
+            button7.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button7.ForeColor = Color.FloralWhite;
+            button7.Location = new Point(143, 342);
+            button7.Margin = new Padding(4);
+            button7.Name = "button7";
+            button7.Size = new Size(111, 91);
+            button7.TabIndex = 6;
+            button7.Text = "X";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.ForestGreen;
+            button8.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button8.ForeColor = Color.FloralWhite;
+            button8.Location = new Point(263, 342);
+            button8.Margin = new Padding(4);
+            button8.Name = "button8";
+            button8.Size = new Size(111, 91);
+            button8.TabIndex = 7;
+            button8.Text = "X";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.YellowGreen;
+            button9.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button9.ForeColor = Color.FloralWhite;
+            button9.Location = new Point(383, 342);
+            button9.Margin = new Padding(4);
+            button9.Name = "button9";
+            button9.Size = new Size(111, 91);
+            button9.TabIndex = 8;
+            button9.Text = "X";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.Window;
+            label1.Location = new Point(83, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 25);
+            label1.TabIndex = 9;
+            label1.Text = "Player:";
+            // 
+            // label2
+            // 
+            label2.AccessibleName = "";
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.Window;
+            label2.Location = new Point(83, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 25);
+            label2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.Window;
+            label3.Location = new Point(83, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 25);
+            label3.TabIndex = 11;
+            label3.Text = "CPU:";
+            // 
+            // playerscore
+            // 
+            playerscore.AutoSize = true;
+            playerscore.ForeColor = SystemColors.Window;
+            playerscore.Location = new Point(181, 51);
+            playerscore.Name = "playerscore";
+            playerscore.Size = new Size(23, 25);
+            playerscore.TabIndex = 12;
+            playerscore.Text = "0";
+            playerscore.Click += label4_Click;
+            // 
+            // cpuscore
+            // 
+            cpuscore.AutoSize = true;
+            cpuscore.ForeColor = SystemColors.Window;
+            cpuscore.Location = new Point(181, 87);
+            cpuscore.Name = "cpuscore";
+            cpuscore.Size = new Size(23, 25);
+            cpuscore.TabIndex = 13;
+            cpuscore.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(607, 562);
+            Controls.Add(cpuscore);
+            Controls.Add(playerscore);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label playerscore;
+        private Label cpuscore;
+    }
+}
---
+++ Ticc Tac Toe/Form1.cs
@@ -0,0 +1,15 @@
+namespace Ticc_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
