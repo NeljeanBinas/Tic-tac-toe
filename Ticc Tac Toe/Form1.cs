eee4546239ed7e9ace7d2fab7951f5e615c71dd3
ActuallyMe-trust <actuallyme136@gmail.com>
2026-09-12 19:45:15 +0800

    feat Add clear game and player actions

--- Ticc Tac Toe/Form1.cs
+++ Ticc Tac Toe/Form1.cs
@@ -2,14 +2,43 @@ namespace Ticc_Tac_Toe
 {
     public partial class Form1 : Form
     {
+        private Button[] buttons;
+
         public Form1()
         {
             InitializeComponent();
+
+            buttons = new[]
+            {
+                button1, button2, button3,
+                button4, button5, button6,
+                button7, button8, button9
+            };
+
+            foreach (var btn in buttons)
+                btn.Click += PlayerMove;
+
+            ClearGame();
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void PlayerMove(object sender, EventArgs e)
         {
+            Button btn = (sender as Button);
 
+            if (btn is Button)
+            {
+                btn.Text = "X";
+                btn.Enabled = false;
+            }
+        }
+
+        private void ClearGame()
+        {
+            foreach (var button in buttons)
+            {
+                button.Text = "";
+                button.Enabled = true;
+            }
         }
     }
 }
